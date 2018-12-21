3fb839b9ceb74fbeb38c8324c7ba8e668c5669e3
Jash Shah <jash.shah20@gmail.com>
2018-12-21 21:34:31 +0530

    Add files via upload

---
+++ Q-Table Algorithm/q_learn.py
@@ -0,0 +1,35 @@
+import gym
+import numpy as np
+
+env = gym.make('FrozenLake-v0')
+
+# initialising table with zeros
+Q = np.zeros([env.observation_space.n,env.action_space.n])
+lr = .8
+y = .95
+num_episodes = 2000
+
+#create list that contain total rewards
+rList = []
+
+for i in range(num_episodes):
+	s = env.reset()
+	rAll = 0
+	d = False
+	j = 0
+	# q table algorithm
+	while j < 99:
+		j+=1
+		a = np.argmax(Q[s,:] + np.random.randn(1,env.action_space.n)*(1./(i+1)))
+		s1,r,d,_ = env.step(a)
+		Q[s,a] = Q[s,a] + lr*(r + y*np.max(Q[s1,:]) - Q[s,a])
+		rAll += r
+		s = s1
+		if d == True:
+			break
+
+	rList.append(rAll)
+
+print "Score over time: " + str(sum(rList)/num_episodes)
+print "Final Q table values "
+print Q
